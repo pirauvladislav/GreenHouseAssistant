16d2c18e7581eb5f65ff3f13095532ddb0cbe2d4
Vladislav Pirau <pirau.vladislav@live.com>
2021-12-17 19:49:57 +0200

    upate nanoff & esp 32 firmware

--- Program.cs
+++ Program.cs
@@ -13,6 +13,15 @@ namespace GreenHouseAssistant
 
         public static void Main()
         {
+            //dotnet tool update -g nanoff
+            //nanoff --update --platform esp32 --serialport COM3 --preview
+
+            ConnectToWiFiRouter();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+        private static void ConnectToWiFiRouter()
+        {
             _boardLed = OnBoardLed.GetOnBoardLed();
 
             WiFiNetworkAdapter adapter = new WiFiNetworkAdapter();
@@ -20,23 +29,9 @@ namespace GreenHouseAssistant
             adapter.OnConnectingError += WiFiAdapter_OnConnectingError;
 
             _boardLed.StartBlinking();
-            adapter.InitWithDHCP("WIFI", "password");
-
-            Thread.Sleep(Timeout.Infinite);
+            adapter.InitWithDHCP("PA-WIFI", "!panfilii@#");
+            //adapter.InitWithDHCP("wifi-ssid", "password");
         }
-
-        private static void WiFiAdapter_OnConnectingError(WiFiConnectionErrorEventArgs e)
-        {
-            Debug.WriteLine($"Wifi connection error: {e.ConnectionError} \n Exception: {e.Exception}");
-            _boardLed.TurnOn();
-        }
-
-        private static void WiFiAdapter_OnConnected(WiFiConnectedEventArgs e)
-        {
-            Debug.WriteLine($"Wifi connected with IP Address: {e.IpAddress}");
-            StartWebServer();
-        }
-
         private static void StartWebServer()
         {
             _boardLed.StartBlinkingFast();
@@ -49,5 +44,16 @@ namespace GreenHouseAssistant
             }
             _boardLed.TurnOn();
         }
+        private static void WiFiAdapter_OnConnectingError(WiFiConnectionErrorEventArgs e)
+        {
+            Debug.WriteLine($"Wifi connection error: {e.ConnectionError} \n Exception: {e.Exception}");
+            _boardLed.TurnOn();
+        }
+        private static void WiFiAdapter_OnConnected(WiFiConnectedEventArgs e)
+        {
+            Debug.WriteLine($"Wifi connected with IP Address: {e.IpAddress}");
+            StartWebServer();
+        }
+
     }
 }
